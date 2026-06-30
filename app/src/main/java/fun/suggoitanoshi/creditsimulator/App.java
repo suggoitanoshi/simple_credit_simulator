3e29ee7719aaecc0633805197481d8b0ca54079c
Fransiskus Febryan Suryawan <franfebryan@gmail.com>
2026-07-01 02:24:34 +0700

    feat: input from file
    add ability to input from file instead of stdin

--- app/src/main/java/fun/suggoitanoshi/creditsimulator/App.java
+++ app/src/main/java/fun/suggoitanoshi/creditsimulator/App.java
@@ -1,5 +1,7 @@
 package fun.suggoitanoshi.creditsimulator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import fun.suggoitanoshi.creditsimulator.controller.SimulatorController;
@@ -11,7 +13,22 @@ public class App {
     CLI cli = new CLI();
     Menu menu = new Menu();
     SimulatorController controller = new SimulatorController(cli, menu);
-    Scanner scanner = new Scanner(System.in);
+    Scanner scanner = null;
+    System.out.println(args.length);
+    System.out.println(args[0]);
+    if(args.length == 1){
+      // input from file
+      try{
+        File inputFile = new File(args[0]);
+        scanner = new Scanner(inputFile);
+      } catch(FileNotFoundException e){
+        System.out.println(e);
+        scanner = new Scanner(System.in);
+      }
+    }
+    if(scanner == null){
+      scanner = new Scanner(System.in);
+    }
     cli.RenderMenu();
     while(menu.getState() != Menu.MenuState.EXIT && scanner.hasNextLine()){
       controller.HandleCommand(scanner.nextLine());
